79c368be257b8e4d95df0d9f1131eb1c9cb03391
Claire Konz <claire.konz@gmail.com>
2021-05-09 11:51:06 -0400

    experience filtering

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,9 @@
-(function($) {
+(function ($) {
   var toggle = document.getElementById("menu-toggle");
   var menu = document.getElementById("menu");
   var close = document.getElementById("menu-close");
 
-  toggle.addEventListener("click", function(e) {
+  toggle.addEventListener("click", function (e) {
     if (menu.classList.contains("open")) {
       menu.classList.remove("open");
     } else {
@@ -11,14 +11,14 @@
     }
   });
 
-  close.addEventListener("click", function(e) {
+  close.addEventListener("click", function (e) {
     menu.classList.remove("open");
   });
 
   // Close menu after click on smaller screens
-  $(window).on("resize", function() {
+  $(window).on("resize", function () {
     if ($(window).width() < 846) {
-      $(".main-menu a").on("click", function() {
+      $(".main-menu a").on("click", function () {
         menu.classList.remove("open");
       });
     }
@@ -44,19 +44,20 @@
     }
   });
 
-  $(".hover").mouseleave(function() {
+  $(".hover").mouseleave(function () {
     $(this).removeClass("hover");
   });
 
-  $(".isotope-wrapper").each(function() {
+  $(".isotope-wrapper").each(function () {
     var $isotope = $(".isotope-box", this);
     var $filterCheckboxes = $('input[type="radio"]', this);
 
-    var filter = function() {
+    var filter = function () {
       var type = $filterCheckboxes.filter(":checked").data("type") || "*";
-      if (type !== "*") {
-        type = '[data-type="' + type + '"]';
-      }
+      // if (type !== "*") {
+      //   type = '[data-type="' + type + '"]';
+      // }
+
       $isotope.isotope({ filter: type });
     };
 
@@ -68,6 +69,29 @@
     $(this).on("change", filter);
     filter();
   });
+
+  // $(".isotope-wrapper").each(function () {
+  //   var $isotope = $(".isotope-box", this);
+  //   var $filterCheckboxes = $('input[type="radio"]', this);
+
+  //   var filter = function () {
+  //     //   var type = $filterCheckboxes.filter(":checked").data("type") || "*";
+  //     //   if (type !== "*") {
+  //     //     type = '[data-type="' + type + '"]';
+  //     //   }
+  //     //   $isotope.isotope({ filter: type });
+  //     var filterValue = $filterCheckboxes.attr('data-type');
+  //     $isotope.isotope({ filter: filterValue });
+  //   };
+
+  //   $isotope.isotope({
+  //     itemSelector: ".isotope-item",
+  //     layoutMode: "masonry"
+  //   });
+
+  //   $(this).on("change", filter);
+  //   filter();
+  // });
 
   lightbox.option({
     resizeDuration: 200,
